776e84b8510d135d52e6ce3468d27cc139535c1c
DESKTOP-LI4L696\Dobrin Marinov <dobrin.marrinov@gmail.com>
2017-04-21 17:54:07 +0300

    Add a Gig form created

---
+++ LiveMusicHub/Controllers/GigsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using LiveMusicHub.Models;
+using LiveMusicHub.ViewModels;
+
+namespace LiveMusicHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
