f5a04720275758d18722259f964249c82dbc533b
DESKTOP-LI4L696\Dobrin Marinov <dobrin.marrinov@gmail.com>
2017-04-21 10:20:04 +0300

    Initial ASP.NET Web API commit

---
+++ LiveMusicHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LiveMusicHub.Startup))]
+namespace LiveMusicHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
