387518dedb45cef3dac329da16d88895b83ce113
DESKTOP-LI4L696\Dobrin Marinov <dobrin.marrinov@gmail.com>
2017-04-21 16:57:05 +0300

    Changes applied to Gig Domain Model

--- LiveMusicHub/Models/Genre.cs
+++ LiveMusicHub/Models/Genre.cs
@@ -1,8 +1,13 @@
-﻿namespace LiveMusicHub.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LiveMusicHub.Models
 {
     public class Genre
     {
         public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
     }
 }
