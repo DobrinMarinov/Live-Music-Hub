b8b48de79745ff1a76746404ac05e6595d9ba048
Dobrin Marinov <dobrin.marrinov@gmail.com>
2017-04-24 22:32:31 +0300

    Login requirement implemented

--- LiveMusicHub/Controllers/GigsController.cs
+++ LiveMusicHub/Controllers/GigsController.cs
@@ -2,6 +2,8 @@
 using System.Web.Mvc;
 using LiveMusicHub.Models;
 using LiveMusicHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 
 namespace LiveMusicHub.Controllers
 {
@@ -14,6 +16,7 @@ namespace LiveMusicHub.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel()
@@ -22,6 +25,27 @@ namespace LiveMusicHub.Controllers
             };
 
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var userId = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == userId);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+
+            var gig = new Gig
+            {
+                ArtistId = arti
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                Genre = genre,
+                Venue = viewModel.Venue
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
