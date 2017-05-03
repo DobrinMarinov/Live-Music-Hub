8469a7bb0fa79d112823723e17057029a981548a
DESKTOP-LI4L696\Dobrin Marinov <dobrin.marrinov@gmail.com>
2017-05-03 12:43:09 +0300

    Merged

--- LiveMusicHub/Controllers/GigsController.cs
+++ LiveMusicHub/Controllers/GigsController.cs
@@ -1,9 +1,13 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using LiveMusicHub.Models;
 using LiveMusicHub.ViewModels;
 using Microsoft.AspNet.Identity;
+<<<<<<< HEAD
 using System;
+=======
+>>>>>>> Saving a Gig implemented, minor changes in database
 
 namespace LiveMusicHub.Controllers
 {
@@ -31,17 +35,29 @@ namespace LiveMusicHub.Controllers
         [HttpPost]
         public ActionResult Create(GigFormViewModel viewModel)
         {
+<<<<<<< HEAD
             var userId = User.Identity.GetUserId();
             var artist = _context.Users.Single(u => u.Id == userId);
+=======
+>>>>>>> Saving a Gig implemented, minor changes in database
             var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
 
             var gig = new Gig
             {
+<<<<<<< HEAD
                 ArtistId = arti
                 DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
                 Genre = genre,
                 Venue = viewModel.Venue
             };
+=======
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+            };
+
+>>>>>>> Saving a Gig implemented, minor changes in database
             _context.Gigs.Add(gig);
             _context.SaveChanges();
 
